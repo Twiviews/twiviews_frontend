92e87af74a063e97d0cedf82b5ab9ef9054d35f1
Shridatta <sgz2@njit.edu>
2020-05-10 12:31:18 -0400

    Added logic for polling and adding into delivery system for search

--- src/contexts/SearchContext/SearchState.js
+++ src/contexts/SearchContext/SearchState.js
@@ -16,8 +16,10 @@ const SearchState = props => {
     const initialState = {
         tweets:[],
         tweet:{},
-        loading:false
+        loading:false,
     }
+
+    let delivSysFlag = 0;
 
     const [state,dispatch] = useReducer(SearchReducer,initialState);
 
@@ -27,11 +29,35 @@ const SearchState = props => {
 
         const res = await axios.get("https://xxeqim55zf.execute-api.us-east-1.amazonaws.com/dev/reviews/"+text);
 
-        dispatch({
-            type:SEARCH_TWEETS,
-            payload:res.data.message
-        })
+        if(res.data.message.length > 0) {
+            dispatch({
+                type:SEARCH_TWEETS,
+                payload:res.data.message
+            })
+            pollGetTweets(text);
+        } else {
+            addToDeliverySystem(text);
+        }
+
+
     };
+
+    const addToDeliverySystem = async (text) => {
+        if(delivSysFlag < 1) {
+        const res = await axios.get("https://xxeqim55zf.execute-api.us-east-1.amazonaws.com/dev/fetch/"+text);
+        console.log(res);
+        delivSysFlag++;
+        pollGetTweets(text);
+        }
+    };
+
+    const pollGetTweets = (text) => {
+        setInterval(()=>{
+            searchTweets(text);
+        },30000);
+    }
+
+    
 
     // Set Loading
     const setLoading = () => dispatch({ type:SET_LOADING });
